0380f6856d33bec4870055b1c2d42a58742eb368
Karan Kumbhare <karankumbhare90@gmail.com>
2025-07-21 19:20:15 +0530

    Local Project Setup

---
+++ app/layout.js
@@ -0,0 +1,26 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins-sans",
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+
+export const metadata = {
+  title: "Interv AI",
+  description: "Interv AI is a platform for creating and managing interviews.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${poppins.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
