b3dfda35ae0441a89b23d985fa17604f307c4b90
Karan Kumbhare <karankumbhare90@gmail.com>
2025-07-21 19:20:15 +0530

    Local Project Setup

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return(
+    <div>
+      <h1>Hello Developer !! <br/> <Button> Let's Start</Button></h1>
+    </div>
+  )
+}
